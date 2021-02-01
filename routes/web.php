6d71569af7503eb70f890f3932f3af777fded14c
Francisco Lopez <franlopbri@gmail.com>
2021-02-01 11:55:44 -0600

    vincula a BD crea controlador y rutas de usuario

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'UserController@index');
+Route::post('users', 'UserController@store')->name('users.store');
+Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
