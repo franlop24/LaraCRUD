26b4b0ec3d4f7d875c2f4e6d7db71d5238b64589
Francisco Lopez <franlopbri@gmail.com>
2021-03-04 12:48:37 -0600

    creacion automarica de usuarios con seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        App\User::create([
+            'name' => 'Admin',
+            'email' => 'i@admin.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        factory(App\User::class, 10)->create();
     }
 }
